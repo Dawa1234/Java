0b9cc69d2f940f4838424475d335947ff6cef094
khangdu <donotdare>
2021-02-23 10:05:09 +0545

    First File

---
+++ Main.java
@@ -0,0 +1,14 @@
+package com.lol;
+
+import java.util.Date;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // This is the explaination for Primitive data types.
+        int a = 1293812832;
+        byte b = -128;
+        long c = 1279312392;
+        System.out.println(a);
+    }
+}
